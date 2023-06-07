cfb6b00bf31b625f246d10f42b73fa4be92316b1
ew938713 <135384480+ew938713@users.noreply.github.com>
2023-06-07 13:33:31 -0500

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ if streamlit.button('Get Fruit Load List'):
    streamlit.dataframe(my_data_rows)
 
 #ADD STOP FOR DEBUG
-#streamlit.stop()
+streamlit.stop()
 
 #allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('Add a fruit')
